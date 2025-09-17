9edb954ec0625359c4c2dcf794275f13e0cb47e1
khalidwalidanwar <86128823+khalidwalidanwar@users.noreply.github.com>
2025-09-17 16:41:00 +0300

    Update home.js

--- script/home.js
+++ script/home.js
@@ -160,17 +160,17 @@ const loadProducts = async (category, subname, containerSelector,zlimit) => {
             card.querySelector(".add-to-cart").addEventListener("click",()=>{
                 document.querySelector(".productPreview").classList.remove("d-none");
                 // load images container
-                document.querySelector(".productPreview .productImagesContainer .productImage img").src = `../sources/${data.imgUrl[0]}`;
+                document.querySelector(".productPreview .productImagesContainer .productImage img").src = `./sources/${data.imgUrl[0]}`;
                 const thumbnailContainer = document.querySelector(".productPreview .productImagesContainer .thumbnailContainer");
                 thumbnailContainer.innerHTML = '';
                 data.imgUrl.forEach((imgUrl, index) => {
                 const thumbnail = document.createElement("img");
-                thumbnail.src = `../sources/${imgUrl}`;
+                thumbnail.src = `./sources/${imgUrl}`;
                 thumbnail.alt = `Thumbnail ${index + 1}`;
                 thumbnail.classList.add("thumbnail");
                 if (index === 0) thumbnail.classList.add("active");
                 thumbnail.addEventListener("click", () => {
-                    document.querySelector(".productPreview .productImagesContainer .productImage img").src = `../sources/${imgUrl}`;
+                    document.querySelector(".productPreview .productImagesContainer .productImage img").src = `./sources/${imgUrl}`;
                     thumbnailContainer.querySelectorAll(".thumbnail").forEach(thumb => thumb.classList.remove("active"));
                     thumbnail.classList.add("active");
                 });
@@ -232,7 +232,7 @@ const loadProducts = async (category, subname, containerSelector,zlimit) => {
                     document.querySelector(".productPreview .closeBtn").click();
                 }else{
                     alert('يرجى تسجيل الدخول أولاً للاضافة إلى سلة التسوق.');
-                    window.location.href = '../components/login/';
+                    window.location.href = './components/login/';
                 }
                 }
             })
@@ -310,7 +310,7 @@ const loadProducts = async (category, subname, containerSelector,zlimit) => {
                     document.querySelector(".productPreview .closeBtn").click();
                 }else{
                     alert('يرجى تسجيل الدخول أولاً للاضافة إلى سلة التسوق.');
-                    window.location.href = '../login/';
+                    window.location.href = './components/login/';
                 }
                 }
             })
@@ -351,7 +351,7 @@ const loadProducts = async (category, subname, containerSelector,zlimit) => {
                     // Here you can add code to actually handle the favorite action (e.g., update a database or local storage)
                 }else{
                     alert('يرجى تسجيل الدخول أولاً لتسجيل الاعجاب.');
-                    window.location.href = '../components/login/';
+                    window.location.href = './components/login/';
                 }
             });
         });
@@ -734,7 +734,7 @@ document.querySelector('.analyse .reviews button').addEventListener('click', asy
                     });
                 }else{
                     alert("بلزم كتابة اسمك في صفحنك الشخصية");
-                    window.location.href='../components/profile';
+                    window.location.href='./components/profile';
                 }
             }
         }else{
@@ -764,4 +764,5 @@ window.addEventListener('scroll', function(e) {
 import {getCookie, setCookie, eraseCookie} from './main.js';
 
 // eraseCookie("user")
+
 
